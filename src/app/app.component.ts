8c20d1827e1b8c6db6983999fbcca90e28256ffa
yunhao chen <w0847446@myscc.ca>
2024-09-17 10:50:18 -0400

    finished assignment with 6 interface instance * create instances in component.ts * adding css in component.css * using ngfor and ngif in component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from '../share/models/User';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'YunhaoChenLearningAngular';
-  name:String = 'yunhao chen';
-  age:number = 18;
-
+  userList: User[] = [
+    {id: 0, firstName: "yunhao", lastName: "chen", age: 18,address: "", isAdmin: true},
+    {id: 1, firstName: "aryan", lastName: "key", age: 19,address: "", isAdmin: false},
+    {id: 2, firstName: "fancis", lastName: "biller", age: 17,address: "", isAdmin: false},
+    {id: 3, firstName: "pratham", lastName: "X", age: 18,address: "", isAdmin: false},
+    {id: 4, firstName: "peter", lastName: "M", age: 22,address: "", isAdmin: false},
+    {id: 5, firstName: "leo", lastName: "burnett", age: 30,address: "", isAdmin: false}
+  ]
 }
