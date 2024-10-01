77c7d73a511d0461b4226f0cde19ec05c7b2bca7
yunhao chen <w0847446@myscc.ca>
2024-09-30 23:29:10 -0400

    Creating service file and movies-content data file

---
+++ src/data/movies-content.ts
@@ -0,0 +1,10 @@
+import {Movies} from '../app/Movies';
+
+export const moviesList : Movies[] =
+  [
+    {name:"The Avengers",yearReleased:2018,director:"Joss Whedon",productionCompany:"Marvel Studios",imageUrl:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTp0qlAoWcOOswIkL_qpjYzJqCCDmWXiBzCXiqbE43Obo8c0Z-s"},
+    {name:"Captain America",yearReleased:2000,director:"Simon",productionCompany:"Marvel Studios",imageUrl:"https://as2.ftcdn.net/v2/jpg/06/02/13/93/1000_F_602139359_ODViPhvIEzwQRPL9KRoIwtKzYuMmpymB.jpg"},
+    {name:"Thor",yearReleased:2011,director:"Kenneth Branagh",productionCompany:"Marvel Studios",imageUrl:"https://as2.ftcdn.net/v2/jpg/05/79/67/83/1000_F_579678392_4Bh9vMB1DP4QXNzE0E2pUHJYLOYpBgwO.jpg"},
+    {name:"Pulp Fiction",yearReleased:1994,director:"Quentin Tarantino",productionCompany:"Miramax",imageUrl:"https://as2.ftcdn.net/v2/jpg/09/70/56/49/1000_F_970564947_qgowmQ5iyagHQIDoFfRoT99cQFJAiQEA.jpg"},
+
+  ];
