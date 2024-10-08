63328f9499d32610b14d59e2485c95750a66abaf
W0847446 <w0847446@myscc.ca>
2024-10-08 09:23:56 -0400

    Define a list of routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from '@angular/router';
+import {MovieListComponent} from "./app/movie-list/movie-list.component";
+import {MovieListItemComponent} from "./app/movie-list-item/movie-list-item.component";
+import {PageNotFoundComponentComponent} from "./app/page-not-found-component/page-not-found-component.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+/*bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));*/
+
+const routes:Routes=[
+  {path:'', redirectTo:'/movies', pathMatch:'full'},//default route
+  {path: 'movies', component: MovieListComponent},
+  {path: 'students/:id', component: MovieListItemComponent},
+  {path:'modify-student', component:MovieListItemComponent},
+  {path:'**', component:PageNotFoundComponentComponent},
+];
+/*bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));*/
+bootstrapApplication(AppComponent, {providers:[provideRouter(routes)]});
