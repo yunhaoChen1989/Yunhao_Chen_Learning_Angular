cdfcb72009adbd46667821cae117d51d9474e8f9
yunhao chen <w0847446@myscc.ca>
2024-10-08 10:36:12 -0400

    Make picture local and show on the page

--- src/data/movies-content.ts
+++ src/data/movies-content.ts
@@ -2,9 +2,9 @@ import {Movies} from '../app/Movies';
 
 export const moviesList : Movies[] =
   [
-    {id:0, name:"The Avengers",yearReleased:2018,director:"Joss Whedon",productionCompany:"Marvel Studios",imageUrl:"../data/image/avengers.jpg"},
-    {id:1, name:"Captain America",yearReleased:2000,director:"Simon",productionCompany:"Marvel Studios",imageUrl:"../data/image/captain.jpg"},
-    {id:2, name:"Thor",yearReleased:2011,director:"Kenneth Branagh",productionCompany:"Marvel Studios",imageUrl:"../data/image/thor.jpg"},
-    {id:3, name:"Pulp Fiction",yearReleased:1994,director:"Quentin Tarantino",productionCompany:"Miramax",imageUrl:"../data/image/pulp.jpg"},
+    {id:0, name:"The Avengers",yearReleased:2018,director:"Joss Whedon",productionCompany:"Marvel Studios",imageUrl:"avengers.jpg"},
+    {id:1, name:"Captain America",yearReleased:2000,director:"Simon",productionCompany:"Marvel Studios",imageUrl:"captain.jpg"},
+    {id:2, name:"Thor",yearReleased:2011,director:"Kenneth Branagh",productionCompany:"Marvel Studios",imageUrl:"thor.jpg"},
+    {id:3, name:"Pulp Fiction",yearReleased:1994,director:"Quentin Tarantino",productionCompany:"Miramax",imageUrl:"pulp.jpg"},
 
   ];
