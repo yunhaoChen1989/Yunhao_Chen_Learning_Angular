58a20f028b2263367293b9eb69ab98887dcd7ce3
W0847446 <w0847446@myscc.ca>
2024-10-08 11:15:32 -0400

    Finished assignment 5 * Fix the first item won't show data by change the id start from 1

--- src/data/movies-content.ts
+++ src/data/movies-content.ts
@@ -2,9 +2,9 @@ import {Movies} from '../app/Movies';
 
 export const moviesList : Movies[] =
   [
-    {id:0, name:"The Avengers",yearReleased:2018,director:"Joss Whedon",productionCompany:"Marvel Studios",imageUrl:"avengers.jpg"},
-    {id:1, name:"Captain America",yearReleased:2000,director:"Simon",productionCompany:"Marvel Studios",imageUrl:"captain.jpg"},
-    {id:2, name:"Thor",yearReleased:2011,director:"Kenneth Branagh",productionCompany:"Marvel Studios",imageUrl:"thor.jpg"},
-    {id:3, name:"Pulp Fiction",yearReleased:1994,director:"Quentin Tarantino",productionCompany:"Miramax",imageUrl:"pulp.jpg"},
+    {id:1, name:"The Avengers",yearReleased:2018,director:"Joss Whedon",productionCompany:"Marvel Studios",imageUrl:"avengers.jpg"},
+    {id:2, name:"Captain America",yearReleased:2000,director:"Simon",productionCompany:"Marvel Studios",imageUrl:"captain.jpg"},
+    {id:3, name:"Thor",yearReleased:2011,director:"Kenneth Branagh",productionCompany:"Marvel Studios",imageUrl:"thor.jpg"},
+    {id:4, name:"Pulp Fiction",yearReleased:1994,director:"Quentin Tarantino",productionCompany:"Miramax",imageUrl:"pulp.jpg"},
 
   ];
