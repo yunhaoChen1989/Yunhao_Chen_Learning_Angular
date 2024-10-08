24c7831d42a404f3dffa648a44653dbe60aba1c6
yunhao chen <w0847446@myscc.ca>
2024-10-08 11:00:25 -0400

    adding click with error

--- src/app/movie-list-item/movie-list-item.component.ts
+++ src/app/movie-list-item/movie-list-item.component.ts
@@ -1,16 +1,38 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Movies} from "../Movies";
-import {NgOptimizedImage} from "@angular/common";
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MovieServiceService} from "../../service/movie-service.service";
+
+
 
 @Component({
   selector: 'app-movie-list-item',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgIf
   ],
   templateUrl: './movie-list-item.component.html',
   styleUrl: './movie-list-item.component.css'
 })
-export class MovieListItemComponent {
+export class MovieListItemComponent implements OnInit{
   @Input() movies?:Movies;
+  showButton:boolean=false
+  constructor(private route: ActivatedRoute, private router: Router, private movieService:MovieServiceService) {
+  }
+  goBack(){
+    this.router.navigate(['movies']);
+  }
+
+  ngOnInit(): void {
+    const id=Number(this.route.snapshot.paramMap.get('id'));
+    //console.log("id",id);
+    if(id){
+      this.showButton=true;
+      this.movieService.getMovieById(id).subscribe(movie=>{
+        this.movies=movie;
+      })
+    }
+  }
 }
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -3,13 +3,15 @@ import {MovieListItemComponent} from "../movie-list-item/movie-list-item.compone
 import {Movies} from "../Movies";
 import {NgForOf} from "@angular/common";
 import {MovieServiceService} from "../../service/movie-service.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
   imports: [
     MovieListItemComponent,
-    NgForOf
+    NgForOf,
+    RouterLink
   ],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
